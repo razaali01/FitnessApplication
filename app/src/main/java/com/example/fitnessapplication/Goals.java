cd59efdbaf831989a9717b4ba5eec24e69f14cb0
razaali01 <57025721+razaali01@users.noreply.github.com>
2022-03-30 13:42:03 -0500

    test1

--- app/src/main/java/com/example/fitnessapplication/Goals.java
+++ app/src/main/java/com/example/fitnessapplication/Goals.java
@@ -2,14 +2,22 @@ package com.example.fitnessapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Goals extends AppCompatActivity implements View.OnClickListener{
 
     private Button Back;
+    EditText textIn;
+    Button buttonAdd;
+    LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,15 +25,38 @@ public class Goals extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
 
-        Back = (Button) findViewById(R.id.BackGoals);
+        textIn = (EditText) findViewById(R.id.textin);
+        buttonAdd = (Button) findViewById(R.id.add);
+        container = (LinearLayout)findViewById(R.id.container);
+
+        Back = (Button) findViewById(R.id.GoalsBack);
         Back.setOnClickListener(this);
+        buttonAdd.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater =
+                        (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View addView = layoutInflater.inflate(R.layout.row, null);
+                TextView textOut = (TextView)addView.findViewById(R.id.textout);
+                textOut.setText(textIn.getText().toString());
+                Button buttonRemove = (Button) addView.findViewById(R.id.remove);
+                buttonRemove.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        ((LinearLayout)addView.getParent()).removeView(addView);
+                    }});
+
+                container.addView(addView);
+            }});
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId())
         {
-            case(R.id.BackGoals):
+            case(R.id.GoalsBack):
                 startActivity(new Intent(this, HomePage.class));
                 break;
         }
--- app/src/main/java/com/example/fitnessapplication/ProfilePage.java
+++ app/src/main/java/com/example/fitnessapplication/ProfilePage.java
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfilePage extends AppCompatActivity implements View.OnClickListener{
 
-    private Button Back;
+    private Button Back, Logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        Logout = (Button) findViewById(R.id.Logout);
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfilePage.this, MainActivity.class));
+            }
+        });
 
         Back = (Button) findViewById(R.id.BackProfile);
         Back.setOnClickListener(this);
