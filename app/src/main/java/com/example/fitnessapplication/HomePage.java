a4b6aa79af530a29b297a1265e51f4492c9a8efd
razaali01 <57025721+razaali01@users.noreply.github.com>
2022-03-30 13:42:03 -0500

    test1

--- app/src/main/java/com/example/fitnessapplication/HomePage.java
+++ app/src/main/java/com/example/fitnessapplication/HomePage.java
@@ -4,11 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomePage extends AppCompatActivity implements View.OnClickListener{
 
@@ -36,6 +44,30 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener{
         ProfilePage = (Button) findViewById(R.id.ProfilePage);
         ProfilePage.setOnClickListener(this);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView greetingTextView = (TextView) findViewById(R.id.Welcome);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile =snapshot.getValue(User.class);
+
+                if(userProfile != null)
+                {
+                    String fullName = userProfile.fullName;
+
+                    greetingTextView.setText("Welcome, " + fullName + "!!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomePage.this, "Something wrong happened", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
