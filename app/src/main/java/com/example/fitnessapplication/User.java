cdeca5268667522dc202ddf2d4b6cb8ecaf19501
razaali01 <57025721+razaali01@users.noreply.github.com>
2022-04-04 14:29:22 -0500

    4/4/2022 2:30 commit java file Reg

--- app/src/main/java/com/example/fitnessapplication/User.java
+++ app/src/main/java/com/example/fitnessapplication/User.java
@@ -2,7 +2,7 @@ package com.example.fitnessapplication;
 
 public class User {
 
-    public String fullName, age, email, password, gender, address;
+    public String fullName, age, email, password, gender, address, height, number;
 
     public User()
     {
@@ -17,5 +17,71 @@ public class User {
         this.password = password;
         this.gender = gender;
         this.address = address;
+        this.height = height;
+        this.number = number;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 }
