b91293765fe0386fcbd83e708b25c871b4cacb77
razaali01 <57025721+razaali01@users.noreply.github.com>
2022-04-26 10:56:31 -0500

    Including Fitness Tracker

---
+++ app/src/main/java/com/example/fitnessapplication/Yoga/WorkoutYogaGroup.java
@@ -0,0 +1,52 @@
+package com.example.fitnessapplication.Yoga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.fitnessapplication.R;
+
+public class WorkoutYogaGroup extends AppCompatActivity implements View.OnClickListener {
+
+    private Button beginner, intermediate, advanced, back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_yoga_group);
+
+        beginner = (Button) findViewById(R.id.YogaBeginner);
+        beginner.setOnClickListener(this);
+
+        intermediate = (Button) findViewById(R.id.YogaIntermediate);
+        intermediate.setOnClickListener(this);
+
+        advanced = (Button) findViewById(R.id.YogaAdvanced);
+        advanced.setOnClickListener(this);
+
+        back = (Button) findViewById(R.id.YogaBack);
+        back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case(R.id.YogaBeginner):
+                startActivity(new Intent(this,YogaBeginner.class));
+                break;
+            case(R.id.YogaIntermediate):
+                startActivity(new Intent(this,YogaIntermediate.class));
+                break;
+            case(R.id.YogaAdvanced):
+                startActivity(new Intent(this, YogaAdvanced.class));
+                break;
+            case(R.id.YogaBack):
+                startActivity(new Intent(this, WorkoutYogaGroup.class));
+                break;
+        }
+    }
+}
